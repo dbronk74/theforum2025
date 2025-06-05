fc83efaf74800fc7b33cb5770c12cba8c7e78da3
Macmini2 <dan.bronk@yahoo.com>
2025-06-05 18:15:43 -0500

    Fix React Router setup to render pages properly

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,8 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function Home() {
+  return <h2>Welcome to The Forum 2025</h2>;
+}
 
 function Gauntlet() {
   return <h2>The Gauntlet - Debate Arena</h2>;
@@ -13,27 +16,23 @@ function WorldSpeak() {
   return <h2>WorldSpeak Arena</h2>;
 }
 
-function Home() {
-  return <h2>Welcome to The Forum 2025</h2>;
-}
-
 function App() {
   return (
     <BrowserRouter>
-      <nav style={{ padding: '1rem', backgroundColor: '#eee' }}>
+      <nav style={{ padding: '1rem', background: '#eee' }}>
         <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
         <Link to="/gauntlet" style={{ marginRight: '1rem' }}>Gauntlet</Link>
         <Link to="/oracle" style={{ marginRight: '1rem' }}>Oracle</Link>
         <Link to="/worldspeak">WorldSpeak</Link>
       </nav>
-      <main style={{ padding: '1rem' }}>
+      <div style={{ padding: '1rem' }}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/gauntlet" element={<Gauntlet />} />
           <Route path="/oracle" element={<Oracle />} />
           <Route path="/worldspeak" element={<WorldSpeak />} />
         </Routes>
-      </main>
+      </div>
     </BrowserRouter>
   );
 }
