dbe78342e4c4a9a057115459a7a2cda517861e6e
Macmini2 <dan.bronk@yahoo.com>
2025-06-05 18:43:33 -0500

    Update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,11 @@
-<!-- Placeholder or // Placeholder for client/vite.config.ts -->
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  root: '.', // Make sure root is current folder
+})
